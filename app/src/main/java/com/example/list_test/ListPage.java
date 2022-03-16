dd6d751dedbd4e3b15f527cb8f5d6a2314100afb
Andrew Thow <thow1976@hotmail.co.uk>
2022-03-16 11:17:59 +0000

    9_setup_recycler_view_widget

--- app/src/main/java/com/example/list_test/ListPage.java
+++ app/src/main/java/com/example/list_test/ListPage.java
@@ -2,7 +2,11 @@ package com.example.list_test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ArrayAdapter;
 
+import com.example.list_test.Adapter.RecyclerViewAdapter;
 import com.example.list_test.data.DatabaseHandler;
 import com.example.list_test.model.Food;
 
@@ -12,7 +16,10 @@ import java.util.List;
 
 public class ListPage extends AppCompatActivity {
 
-    //private ArrayList<String> foodArrayList;
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter recyclerViewAdapter;
+    private ArrayList<String> foodArrayList;
+    private ArrayAdapter<String> arrayAdapter;
 
    //public ListPage(ArrayList<String> foodArrayList) {
     // this.foodArrayList = foodArrayList;
@@ -22,7 +29,14 @@ public class ListPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_page_activity);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        foodArrayList = new ArrayList<>();
         DatabaseHandler db = new DatabaseHandler(ListPage.this);
+
 
         //List<Food> foodList = db.getAllFoodItems();
         //for(Food food: foodList) {
